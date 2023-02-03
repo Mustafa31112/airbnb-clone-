f25421e8d78ec9b4216b56c1a39326847a5130d7
Mustafa <mustafa31112@gmail.com>
2023-02-03 01:20:23 -0500

    added different components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
+import React from "react";
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// eslint-disable-next-line
+import Search from "@mui/icons-material/Search";
+
 function App() {
   return (
     //BEM
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
 
-      {/* Header */}
+        <Switch>
+          <Route path="/search">
+          <SearchPage />
+          </Route>
+          <Route path="/">
+          <Home />
+          </Route>
+        </Switch>
 
-      {/* Banner */}
+        <Footer />
+      </Router>
     </div>
   );
 }
